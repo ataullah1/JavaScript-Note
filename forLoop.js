7e5d881d389b05b927372070e0972d15b7c57d2d
Md Ataullah <ataullahm100@gmail.com>
2024-01-30 10:00:25 +0600

    For Loop New Practices Added

--- forLoop.js
+++ forLoop.js
@@ -7,3 +7,16 @@ for (let i = 1; i <= 10; i++) {
     console.log(i);
   }
 }
+
+for (let i = 1; i < 15; i += 2) {
+  console.log(i);
+}
+
+let sum = 0;
+for (let i = 0; i < 30; i++) {
+  if (i % 3 === 0) {
+    console.log(i);
+    sum += i;
+  }
+}
+console.log('Total: ', sum);
