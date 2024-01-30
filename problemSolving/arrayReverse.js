1c75862e4b2ddda2e95f22d5da718fce3200d0e0
Md Ataullah <ataullahm100@gmail.com>
2024-01-30 23:29:17 +0600

    Problem Sloveing

---
+++ problemSolving/arrayReverse.js
@@ -0,0 +1,23 @@
+let arrayRevers = ['Banana', 'Apple', 'Coconut', 'Grape', 'Mango'];
+
+for (let i = arrayRevers.length - 1; i >= 0; i--) {
+  console.log(arrayRevers[i]);
+}
+
+console.log('======================');
+// Sum for loop
+let sumArrayRevers = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50];
+sum = 0;
+for (let i = 0; i < sumArrayRevers.length; i++) {
+  const element = sumArrayRevers[i];
+  sum += element;
+}
+console.log(sum);
+
+console.log('===========================');
+
+let a = [5, 10, 15, 20, 25];
+let temp = a.length - 1;
+temp = a[1];
+a[1] = temp;
+console.log(a);
