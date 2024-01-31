da72e192425904df07450c37a2d75f90dc5539f8
Md Ataullah <ataullahm100@gmail.com>
2024-01-31 16:35:48 +0600

    Practices Update

--- case.js
+++ case.js
@@ -3,3 +3,9 @@ let a = '       Amar sonar bangla amit tomay Valobashi.';
 console.log(a.trim());
 console.log(a.toUpperCase());
 console.log(a.toLowerCase());
+
+console.log('------------');
+
+const x = 'Hello';
+x[0] = 'h';
+console.log(x);
