507f6ea954b88d1c0faf0541ba5ab8c8d131e015
Md Ataullah <ataullahm100@gmail.com>
2024-01-31 13:30:10 +0600

    Object learning

---
+++ objectNested.js
@@ -0,0 +1,17 @@
+const college = {
+  name: 'Hatem Ali',
+  class: ['10', '11', '12', '13', '14', '15'],
+  events: ['science fair', 'bijoy dibos', '21 feb'],
+  unique: {
+    color: 'blue',
+    result: {
+      gpa: 5,
+      merit: 'top',
+    },
+  },
+};
+
+console.log(college);
+
+console.log(college.unique);
+console.log(college.unique.result);
