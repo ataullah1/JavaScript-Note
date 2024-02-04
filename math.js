34d8f5b1b0a1828e99202d768f8217bd6e5dee41
Md Ataullah <ataullahm100@gmail.com>
2024-02-04 16:30:41 +0600

    Math Function

---
+++ math.js
@@ -0,0 +1,27 @@
+// Sobcea boro number ta dibe
+console.log(Math.max(232, 5, 43, 6, 457, 65, 78, 5));
+
+// Sobcea soto number ta dibe.
+console.log(Math.min(232, 5, 43, 6, 457, 65, 78, 5));
+
+// PI ar man dibe
+console.log(Math.PI);
+
+// Nagetive Value ke Positive value kore dibe and aker odik value thakle sob gulake sum kore kore dibe .
+console.log(Math.abs(-45 - 46));
+
+// Vognongso number ke purno songkha kore dibe and .5 ar upre thakle porer songkha hobe ar nice hole oi sonkhay thakbe .
+console.log(Math.round(15.7));
+
+// Vognansho sonkhake purno sonkha korbe tobe seta sudu je sonkha thakbe oty takbe sudu (.) ar porer sonkhata kete dibe
+console.log(Math.floor(34.7));
+
+// Vognansho sonkhake purno sonkha korbe (.) ar por ja thakuk(0 ar cea boro) o porer sonkha output dibe.
+console.log(Math.ceil(34.1));
+
+// 0 theke 1 ar modde Random number dibe..
+console.log(Math.random());
+
+//  Random number o/1 ar cea besi pete..(random ke akta variable a nia terpor detake abr round a dia purno sonkha kore nite hobe)
+const random = Math.random() * 10;
+console.log(Math.round(random));
