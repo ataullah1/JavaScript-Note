03242aeb92b8dd432a8306b3ab9fcd5f105d3c4c
Md Ataullah <ataullahm100@gmail.com>
2024-02-05 10:26:13 +0600

    Calculator add

---
+++ problemSolving/calculator.js
@@ -0,0 +1,33 @@
+function add(add1, add2) {
+  return add1 + add2;
+}
+function subtract(add1, add2) {
+  return add1 - add2;
+}
+function multiply(add1, add2) {
+  return add1 * add2;
+}
+function divide(add1, add2) {
+  return add1 / add2;
+}
+
+function calculator(a, b, operator) {
+  if (operator === 'sum') {
+    return add(a, b);
+  } else if (operator === 'subtract') {
+    return subtract(a, b);
+  } else if (operator === 'multiply') {
+    return multiply(a, b);
+  } else if (operator === 'divide') {
+    return divide(a, b);
+  } else {
+    return 'Please Input Valid Input';
+  }
+}
+const w = 'sum';
+const x = 'subtract';
+const y = 'multiply';
+const z = 'divide';
+const opret = w;
+const output = calculator(5, 4, w);
+console.log(calculator(output));
