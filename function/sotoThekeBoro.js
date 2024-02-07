61262785bc14ee97c20043193347ed45106ae1e6
Md Ataullah <ataullahm100@gmail.com>
2024-02-07 14:30:15 +0600

    Sort Function for js

--- function/sotoThekeBoro.js
+++ function/sotoThekeBoro.js
@@ -1,3 +1,70 @@
-function sort(arr) {
-    
+const sortArr = [235, 64, 75, 8, 9, 690, 68, 456, 45];
+sortArr.sort((a, b) => a - b);
+console.log(sortArr);
+
+console.log('---------');
+const sortArrFlo = [2.34, 1.344, 6.456, 4.234, 9.34];
+sortArrFlo.sort((a, b) => a - b);
+console.log(sortArrFlo);
+
+console.log('---------');
+
+const sortArrNeg = [
+  -6,
+  -2,
+  -3,
+  34.56,
+  72.34,
+  2.565,
+  4 - 98,
+  -23,
+  -1,
+  3,
+  6,
+  0,
+  32,
+  6,
+];
+sortArrNeg.sort((a, b) => a - b);
+console.log(sortArrNeg);
+
+console.log('---------');
+
+/** 
+function sortArray(arr) {
+  //   return arr.slice().sort((a, b) => a - b);
+  console.log(arr.slice().sort((a, b) => a - b));
 }
+
+// Test with varying lengths and types of arrays
+const arrays = [
+  [5, 2, 9, 1, 11, 6], // All integers
+  [-3, -7, -1, -9, -2], // All negative numbers
+  [3.14, 1.618, 2.718, 0.577], // All decimals
+];
+
+arrays.forEach((array) => {
+  const sortedArray = sortArray(array);
+  //   console.log(`Original array: [${array}]`);
+  //   console.log(`Sorted array: [${sortedArray}]`);
+  //   console.log();
+});
+*/
+function srt(a, b) {
+  return a - b;
+}
+const arr = [2, 5, 78, 2, 4, 11, 3, 1];
+function sorted(array) {
+  const sored = array;
+  /** 
+  sored.sort(function (a, b) {
+    return a - b;
+  }); 
+ || or ||
+  sored.sort((a, b) => a - b);
+  */
+  sored.sort((a, b) => a - b);
+  return sored;
+  //   console.log(sored);
+}
+console.log(sorted(arr));
