a4fad3b8c2c7a25f277cd14eefa5b6099847f83c
Md Ataullah <ataullahm100@gmail.com>
2024-02-23 08:10:35 +0600

    destructuring array

--- ES6/Destructure.js
+++ ES6/Destructure.js
@@ -6,3 +6,25 @@ const actor = {
 };
 const phn = actor.phone;
 console.log(phn);
+
+// destructuring object
+console.log('Hello');
+const player = {
+  name: 'Sakib',
+  age: 50,
+  phone: '01234282829',
+  mony: '33',
+};
+const { age, phone, mony: bow } = player;
+console.log(age);
+console.log(phone);
+console.log(bow);
+
+//destructuring array
+const arr = [32, 456];
+const [first, second] = arr;
+const [a, b] = [12, 66];
+console.log(arr);
+console.log(first);
+console.log(a);
+console.log(b);
