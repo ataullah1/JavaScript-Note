7f2dec32252d573429b31d214184442c68bff4b2
Md Ataullah <ataullahm100@gmail.com>
2024-02-23 07:37:47 +0600

    arrowFunction add

---
+++ ES6/arrowFunction.js
@@ -0,0 +1,29 @@
+const arrow = () => 'hello';
+console.log(arrow());
+
+const arrow1 = (a) => 'hello Arrow 1';
+console.log(arrow1());
+
+const arrow2 = (a, b, c) => a + b * c;
+console.log(arrow2(2, 4, 6));
+
+const arrow3 = (a) => a.age;
+const obj = {
+  name: 'Ataullah',
+  age: 18,
+};
+console.log(arrow3(obj));
+
+const pi = () => Math.PI;
+console.log(pi());
+
+const getMath = (a, b, c, d) => {
+  const ab = a;
+  const ac = b;
+  const ad = c;
+  const ae = a + b + c;
+  const af = a + b + c + d;
+  const ag = ab + ac + ad;
+  return af;
+};
+console.log(getMath(2, 4, 6, 7));
