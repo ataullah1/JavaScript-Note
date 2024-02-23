2d7cf0fff1406ebdb7497d34d80197935d7b0586
Md Ataullah <ataullahm100@gmail.com>
2024-02-23 22:16:16 +0600

    reduce

--- ES6/reduce.js
+++ ES6/reduce.js
@@ -1,3 +1,23 @@
 const num = [42, 34, 67, 89, 53, 74, 83, 56];
 const redu = num.reduce((pre, curr) => pre + curr, 0);
 console.log(redu);
+
+console.log('============');
+
+const people = [
+  { name: 'Meena', age: 20 },
+  { name: 'Rina', age: 15 },
+  { name: 'Suchorita', age: 22 },
+];
+//Reduce
+const total = people.reduce((p, c) => p + c.age, 0);
+console.log(total);
+
+console.log('===========');
+
+//For Of loop
+let sum = 0;
+for (const p of people) {
+  sum += p.age;
+}
+console.log(sum);
