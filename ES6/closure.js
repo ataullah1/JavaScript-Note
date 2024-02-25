b602a72536fc60404f06fd714b135cb85d9d809f
Md Ataullah <ataullahm100@gmail.com>
2024-02-25 09:03:44 +0600

    CLosure

---
+++ ES6/closure.js
@@ -0,0 +1,13 @@
+const watch = stopWatch();
+console.log(watch());
+console.log(watch());
+console.log(watch());
+console.log(watch());
+// =======
+
+const c = counting();
+// protekber call korle 1 kore barbe
+console.log(c()); //1
+console.log(c()); //2
+console.log(c()); //3
+console.log(c()); //4
