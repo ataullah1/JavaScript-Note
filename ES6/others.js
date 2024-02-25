f49aa91b2b8621f6e7d5005f3d8e96e911b3857e
Md Ataullah <ataullahm100@gmail.com>
2024-02-25 09:27:01 +0600

    true and false value

--- ES6/others.js
+++ ES6/others.js
@@ -2,3 +2,15 @@ const a = 'I am a web developer.';
 const b = 'I love to code.';
 const c = 'I love to eat biryani.';
 console.log(`${a} ${b} ${c}`);
+
+const ab = 'Ataullah';
+const ac = {};
+const ad = [];
+const ae = 4;
+const af = ' ';
+const aff = '';
+const ag = true;
+const ah = false;
+const ai = undefined;
+const aj = null;
+console.log(!!ab, !!ac, !!ad, !!ae, !!af, !!aff, !!ag, !!ah, !!ai, !!aj);
