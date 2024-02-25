40c46154d1552282439d82dd9fdc18a5079c4284
Md Ataullah <ataullahm100@gmail.com>
2024-02-25 17:44:38 +0600

    FOr Each

--- ES6/forEach.js
+++ ES6/forEach.js
@@ -3,3 +3,6 @@
 const a = [, 645, 7, 568, 67, 9];
 const result = a.forEach((n) => n * 2);
 console.log(result);
+
+const num = [235, 346, 5764, 78, 568, 967, 96, 345];
+num.forEach((item) => console.log(item));
