34dd433befe6028ef80adb13d0cf361ba4b43fee
Md Ataullah <ataullahm100@gmail.com>
2024-02-26 22:03:51 +0600

    JSON

---
+++ JSON/convertObjAndString.js
@@ -0,0 +1,12 @@
+const jsn = {
+  name: 'Kodu Azad',
+  age: 29,
+  adress: {
+    name: 'Kocukhet',
+    mitter: 234,
+  },
+};
+const a = JSON.stringify(jsn);
+const b = JSON.parse(a);
+console.log(a);
+console.log(b);
