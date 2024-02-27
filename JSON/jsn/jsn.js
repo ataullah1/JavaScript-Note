6d1960f77ec983124d2e259a63980236150876bf
Md Ataullah <ataullahm100@gmail.com>
2024-02-27 07:45:48 +0600

    Api data to html list

--- JSON/jsn/jsn.js
+++ JSON/jsn/jsn.js
@@ -6,3 +6,20 @@ function jsnToObj() {
     .then((response) => response.json())
     .then((json) => console.log(json));
 }
+// ==================================
+
+document.getElementById('btn1').addEventListener('click', function () {
+  const link = 'https://jsonplaceholder.typicode.com/posts';
+  fetch(link)
+    .then((res) => res.json())
+    .then((data) => apiCall(data));
+  function apiCall(dataList) {
+    const ul = document.getElementById('titleList');
+    for (const data of dataList) {
+      // console.log(data);
+      const li = document.createElement('li');
+      li.innerText = data.title;
+      ul.appendChild(li);
+    }
+  }
+});
