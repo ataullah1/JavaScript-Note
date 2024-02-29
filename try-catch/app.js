200efe52665af1768ae014e6ebbc899afea46848
Md Ataullah <ataullahm100@gmail.com>
2024-02-29 17:23:06 +0600

    try catch

--- try-catch/app.js
+++ try-catch/app.js
@@ -1,1 +1,21 @@
 console.log('Hello');
+document.getElementById('btn').addEventListener('click', () => {
+  const input = document.getElementById('inputFild').value;
+  const errorMassage = document.getElementById('error');
+  try {
+    const inpNum = parseInt(input);
+    if (isNaN(inpNum)) {
+      throw 'Please input a valid number age.';
+    } else if (inpNum < 18) {
+      throw 'Tmi Bassa manus ar aktu boro hoa nao.';
+    } else if (inpNum > 60) {
+      throw 'Murubbi apner to boios hoa gese.';
+    }
+    errorMassage.innerText = '';
+  } catch (err) {
+    errorMassage.innerText = err;
+    console.log(err);
+  } finally {
+    console.log('Hi Kmn Aso?');
+  }
+});
