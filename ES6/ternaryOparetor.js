ec2c822ff5cb38b5d32c6f8a5a4440078fc5ac49
Md Ataullah <ataullahm100@gmail.com>
2024-03-07 11:39:02 +0600

    Ternary oparetor

---
+++ ES6/ternaryOparetor.js
@@ -0,0 +1,8 @@
+const mny = 150;
+if (mny > 100) {
+  console.log('Biriany khabo');
+} else {
+  console.log('Biskut Khabo');
+}
+const food = mny > 120 ? 'Gril Biriani khabo' : 'Cha biskit khwa cara upay nay';
+console.log(food);
