9d47482e5bea2456ddaaae1bda4a669f600a5df7
Md Ataullah <ataullahm100@gmail.com>
2024-03-07 12:39:03 +0600

    Object Bracate notation

--- object.js
+++ object.js
@@ -15,3 +15,15 @@ console.log(a.lastName);
 
 // Bracket dia access korar niom======
 console.log(a['Age']);
+console.log('===========');
+
+const obj = {
+  name: 'Ataullah',
+  age: 18,
+  'local address': 'Uttar Kakchira',
+};
+
+const output = obj['local address'];
+const output2 = obj['age'];
+const output3 = obj['name'];
+console.log(output, output2, output3);
