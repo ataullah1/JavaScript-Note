7d614512232994a28fdf35aca6b4eb7a4cb81882
Md Ataullah <ataullahm100@gmail.com>
2024-04-17 18:37:19 +0600

    Js start

--- DOM/Count down time/app.js
+++ DOM/Count down time/app.js
@@ -1,1 +1,9 @@
 console.log('Hello world');
+const day = document.getElementById('day');
+const hour = document.getElementById('hour');
+const minute = document.getElementById('minute');
+const second = document.getElementById('second');
+const date = new Date();
+
+// console.log(day, hour, minute, second);
+console.log(date.getDate());
