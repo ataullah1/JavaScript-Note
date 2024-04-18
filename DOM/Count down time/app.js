fb3c1466ce10c6714e31133d6988a0ab537cf592
Md Ataullah <ataullahm100@gmail.com>
2024-04-18 18:12:49 +0600

    JS

--- DOM/Count down time/app.js
+++ DOM/Count down time/app.js
@@ -3,7 +3,38 @@ const day = document.getElementById('day');
 const hour = document.getElementById('hour');
 const minute = document.getElementById('minute');
 const second = document.getElementById('second');
-const date = new Date();
 
 // console.log(day, hour, minute, second);
-console.log(date.getDate());
+
+// const dateTime = () => {
+//   const date = new Date();
+//   const dateDay = date.getDate();
+//   const gonta = date.getHours();
+//   const mnt = date.getMinutes();
+//   const scnd = date.getSeconds();
+//   day.innerText = dateDay;
+//   hour.innerText = gonta;
+//   minute.innerText = mnt;
+//   second.innerText = scnd;
+// };
+// setTimeout(dateTime(), 1000);
+
+function timer() {
+  let currentTime = new Date();
+  let date = currentTime.getDate();
+  let hours = currentTime.getHours();
+  let minutes = currentTime.getMinutes();
+  let sec = currentTime.getSeconds();
+  if (minutes < 10) {
+    minutes = '0' + minutes;
+  }
+  if (sec < 10) {
+    sec = '0' + sec;
+  }
+
+  day.innerText = date;
+  hour.innerText = hours;
+  minute.innerText = minutes;
+  second.innerText = sec;
+  setTimeout(timer, 1000);
+}
